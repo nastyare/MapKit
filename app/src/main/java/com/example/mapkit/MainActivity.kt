8a572a69c8a8a3a3ea98e8b6861992cf1c5f57a2
Anastasia <anastasiya.reksius@mail.ru>
2024-05-17 21:16:37 +0700

    done

--- app/src/main/java/com/example/mapkit/MainActivity.kt
+++ app/src/main/java/com/example/mapkit/MainActivity.kt
@@ -1,60 +1,95 @@
 package com.example.mapkit
 
+import android.content.pm.PackageManager
+import android.graphics.PointF
 import android.os.Bundle
 import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKit
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.map.InputListener
+import com.yandex.mapkit.map.MapObjectCollection
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.runtime.image.ImageProvider
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mapView: MapView
-    private lateinit var locBtn: FloatingActionButton
     private lateinit var toggleBtn: ToggleButton
+    private lateinit var myLocation: FloatingActionButton
+    private lateinit var mapCollection: MapObjectCollection
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MapKitFactory.setApiKey("0124477f-8ee9-4fc5-901f-fea07a74717e")
         setContentView(R.layout.activity_main)
         MapKitFactory.initialize(this)
-        setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapview)
 
-        locBtn = findViewById(R.id.location)
-        locBtn.setOnClickListener() {
-            val (latitude, longitude) = glavPochtamt()
+
+        var mapKit: MapKit = MapKitFactory.getInstance()
+
+        getLocationPermision()
+
+        //для пробок
+        var traffic = mapKit.createTrafficLayer(mapView.mapWindow)
+        var trafficIsOn = false
+        toggleBtn = findViewById(R.id.toggleBtn)
+        toggleBtn.setOnClickListener {
+            when(trafficIsOn) {
+                false -> {
+                    trafficIsOn = true
+                    traffic.isTrafficVisible = true
+                }
+                true -> {
+                    trafficIsOn = false
+                    traffic.isTrafficVisible = false
+                }
+            }
+        }
+
+        //показ локации текущей
+        myLocation = findViewById(R.id.location)
+        myLocation.setOnClickListener() {
+            var location = mapKit.createUserLocationLayer(mapView.mapWindow)
+            location.isVisible = true
             mapView.map.move(
-                CameraPosition(Point(latitude, longitude), 18.0f, 0.0f, 0.0f),
+                CameraPosition(Point(55.351735295422394,86.09376724603264), 16f, 0.0f, 0.0f),
                 Animation(Animation.Type.SMOOTH, 2F),
                 null
             )
         }
-        var mapKit: MapKit = MapKitFactory.getInstance()
-        var probki = mapKit.createTrafficLayer(mapView.mapWindow)
-        var probkiIsOn = false
-        toggleBtn = findViewById(R.id.toggleBtn)
-        toggleBtn.setOnClickListener {
-            when(probkiIsOn) {
-                false -> {
-                    probkiIsOn = true
-                    probki.isTrafficVisible = true
-                }
-                true -> {
-                    probkiIsOn = false
-                    probki.isTrafficVisible = false
-                }
-            }
+
+        mapCollection = mapView.map.mapObjects.addCollection()
+        mapView.map.addInputListener(inputListener)
+
+    }
+
+    private val inputListener = object : InputListener {
+        override fun onMapTap(p0: Map, p1: Point) {
+            setMark(p1)
+        }
+
+        override fun onMapLongTap(p0: Map, p1: Point) {
+            setMark(p1)
         }
     }
-
-    fun glavPochtamt(): Pair<Double, Double> {
-        return Pair(55.35485859365324, 86.0863204841308)
+    //обработка нажатия
+    fun setMark(point: Point) {
+        mapCollection.addPlacemark(point)
     }
 
-
+    private fun getLocationPermision() {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION,), 0)
+            return
+        }
+    }
     override fun onStart() {
         super.onStart()
         MapKitFactory.getInstance().onStart()
